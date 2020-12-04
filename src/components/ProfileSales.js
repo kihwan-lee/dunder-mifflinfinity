38aaf9aa1e9ab2ff0db04e958e534104c7afc6a3
kihwan-lee <kihwanlee97@gmail.com>
2020-12-04 01:42:44 -0600

    Removed comment

--- src/components/ProfileSales.js
+++ src/components/ProfileSales.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const SalesCard = (props) => (
   <div className="col-md-4">
     <div className="card mb-4 shadow-sm">
-      {/* <img src={`https://robohash.org/${props.contact.name}.png`} className="card-img-top" alt="Jane Doe" width="100" /> */}
       <div className="card-body">
       <h5 className="card-title">Sales</h5>
         <p className="card-text">{props.sales}</p>
