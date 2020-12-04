dddee6ecbf246c2071c581ad373aec14c8a4589a
kihwan-lee <kihwanlee97@gmail.com>
2020-12-04 02:52:26 -0600

    Made Header for sign up page

--- src/pages/NewEmployee.js
+++ src/pages/NewEmployee.js
@@ -4,6 +4,10 @@ import SignUp from '../components/Signup';
 const NewEmployee = (props) => {
   return (
     <div>
+      <div className="registration-header">
+      <h1>Welcome to Dunder Mifflin</h1>
+      <h3>The World's Best Paper Company!</h3>
+      </div>
       <SignUp  setUser={props.setUser} />
 
     </div>
