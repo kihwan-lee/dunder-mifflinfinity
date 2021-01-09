f20e9f6a8b2ac2d82a0c0a307d634dcfab1a134d
kihwan-lee <kihwanlee97@gmail.com>
2021-01-09 00:54:33 -0600

    Styled three column layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import NavBar from '../components/Navbar';
 import BranchCard from '../components/BranchCard';
@@ -9,10 +10,20 @@ const Home = () => {
     <div>
       <NavBar />
       <h1 className="welcome" style={{fontWeight: 900}}>Dunder Mifflinfinity</h1>
+      {/* <BranchCard />
       <BranchCard />
-      <BranchCard />
-      <BranchCard />
-
+      <BranchCard /> */}
+      <div className="row">
+        <Link to="/" className="office-location-column">
+          <svg className="bd-placeholder-img card-img-top" width="10%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title></title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Stamford</text></svg>
+        </Link>
+        <Link to="/" className="office-location-column">
+          <svg className="bd-placeholder-img card-img-top" width="10%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title></title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Scranton</text></svg>
+        </Link>
+        <Link to="/" className="office-location-column">
+          <svg className="bd-placeholder-img card-img-top" width="10%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title></title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Dallas</text></svg>
+        </Link>
+      </div>
 
     </div>
   );
